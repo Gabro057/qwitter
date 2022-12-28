3121e8069bb54e2614f0cc3d0ccaf754435334db
Jan Hrbek <hrbekj@email.cz>
2022-12-28 20:03:43 +0100

    Main page - add qweet section

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,63 @@
 <template>
   <q-page>
-    <h5>Home</h5>
+    <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+      <div class="col">
+        <q-input
+          class="new-qweet"
+          bottom-slots
+          v-model="newQweetContent"
+          placeholder="What's happening?"
+          counter
+          maxlength="280"
+          autogrow
+        >
+          <template v-slot:before>
+            <q-avatar size="xl">
+              <img
+                src="https://gravatar.com/avatar/2ed6381339dc2f0cd3afd0658f0269e3?s=80"
+              />
+            </q-avatar>
+          </template>
+          <template v-slot:append>
+            <q-icon
+              v-if="newQweetContent !== ''"
+              name="close"
+              @click="newQweetContent = ''"
+              class="cursor-pointer"
+            />
+          </template>
+        </q-input>
+      </div>
+      <div class="col col-shrink">
+        <q-btn
+          color="primary"
+          class="q-mb-lg"
+          label="Qweet"
+          no-caps
+          rounded
+          unelevated
+          :disable="!newQweetContent"
+        />
+      </div>
+    </div>
+
+    <q-separator class="divider" size=".8rem" color="grey-2" />
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup>
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "IndexPage",
-});
+const newQweetContent = ref("");
 </script>
+
+<style lang="sass">
+.new-qweet textarea
+  font-size: 120%
+  line-height: 1.4 !important
+
+.divider
+  border-top: 1px solid
+  border-bottom: 1px solid
+  border-color: $grey-4
+</style>
