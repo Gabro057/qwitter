5d1959d054300f02a17d60e58efbb225346fad6c
Jan Hrbek <hrbekj@email.cz>
2022-12-28 08:41:23 +0100

    Creating About page

--- src/pages/PageAbout.vue
+++ src/pages/PageAbout.vue
@@ -1,6 +1,18 @@
 <template>
   <q-page padding>
-    <h5>PageAbout</h5>
+    <h4 class="q-mt-none q-mb-md text-weight-bold">About Qwitter</h4>
+    <div class="text-body1">
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas eum voluptatum
+        magnam qui voluptatem unde a, nam, deserunt ipsa, eius libero nisi quam dolorem
+        dignissimos soluta! Exercitationem eaque, sint ex labore eligendi saepe? Maiores,
+        tempora.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos quo voluptate
+        eveniet ratione quis nulla impedit quas fugiat!
+      </p>
+    </div>
   </q-page>
 </template>
 
